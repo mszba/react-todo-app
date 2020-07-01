2c774934bd3ab436f91729a293c5c326e8f578cc
Marek Szuba <mszuba01@gmail.com>
2020-07-01 23:15:35 +0200

    Updates

---
+++ precache-manifest.2afde11824bd90bf2c6e57bc19d6a766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107a14d2ed42ecefbab970be060d2dca",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "5cb4c4b13cc9897a64bc",
+    "url": "/react-todo-app/static/css/main.1c98303b.chunk.css"
+  },
+  {
+    "revision": "f061105271fd6410e31c",
+    "url": "/react-todo-app/static/js/2.0a99dbe8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.0a99dbe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb4c4b13cc9897a64bc",
+    "url": "/react-todo-app/static/js/main.ca9f6c32.chunk.js"
+  },
+  {
+    "revision": "19f5f32b7ba24305e70c",
+    "url": "/react-todo-app/static/js/runtime-main.c3bcc63f.js"
+  }
+]);
